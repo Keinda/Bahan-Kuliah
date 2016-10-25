7d7db04eeef3de949983eed9f89a9079866145f7
Keinda Dwi Adilia <kda1811@gmail.com>
2016-10-25 14:14:28 +0700

    Update JualanLemonadeYay.pas

--- JualanLemonadeYay.pas
+++ JualanLemonadeYay.pas
@@ -1,110 +1,118 @@
 program JualanLemonadeYay;
 var
-	harga, n, jumlah, pil, time, stokgula, stoklemon, stokgelas, stoksedotan : integer;
-	
-procedure Sale(n : integer, var harga, jumlah : integer);
+	harga, n, jumlah, pil, time, stokgula, stoklemon, stokgelas, stoksedotan, restockvalue : longint;
+	key : char;
+
+procedure Sale(n : longint; var harga, jumlah : longint);
 var
-	duit : integer;
+	duit : longint;
 begin
 	harga := 5000;
 	jumlah := n*harga;
-	writeln("Anda harus bayar sebesar Rp ",jumlah);
-	write("Anda akan bayar dengan uang : Rp ");
+	writeln('Anda harus bayar sebesar Rp ',jumlah);
+	write('Anda akan bayar dengan uang : Rp ');
 	readln(duit);
-	duit := 100000 - duit;
-	writeln("Kembaliannya adalah Rp ",duit);
-end.
+	duit := duit - jumlah;
+	writeln('Kembaliannya adalah Rp ',duit);
+end;
 
-procedure Production(n, harga, stokgula, stoklemon, stokgelas, stoksedotan : integer);
+procedure Production(n, stokgula, stoklemon, stokgelas, stoksedotan, restockvalue : longint);
 var
-	gula, lemon, gelas, sedotan, restockgula, restocklemon, restockgelas, restocksedotan : integer;
+	gula, lemon, gelas, sedotan, restockgula, restocklemon, restockgelas, restocksedotan : longint;
 begin
 	{Deklarasi Awal}
 	gula := 0;
 	lemon := 0;
 	gelas := 0;
 	sedotan := 0;
+	restockgula := 0;
+	restocklemon := 0;
+	restockgelas := 0;
+	restocksedotan := 0;
 	{Data Bahan / Produksi}
 	gula := 1*n;
 	lemon := 1*n;
 	gelas := 1*n;
 	sedotan := 1*n;
-	{Store Checking}
+	{Store Checking and Restocking}
 	stokgula := stokgula - gula;
-	stoklemon := stoklemon - lemon;
-	stokgelas := stokgelas - gelas;
-	stoksedotan := stoksedotan - sedotan;
-	{Harga Restocking}
 	if stokgula < 1 then
 	begin
 		restockgula := 10000;
-		stokgula := stokgula + 100;
-	end;
+		stokgula := 100 + stokgula;
+	end else stokgula := 0 + stokgula;
+	stoklemon := stoklemon - lemon;
 	if stoklemon < 1 then
 	begin
-		restocklemon := 20000*20;
-		stoklemon := stoklemon + 100;
-	end;
+		restocklemon := 400000;
+		stoklemon := 100 + stoklemon;
+	end else stoklemon := 0 + stoklemon;
+	stokgelas := stokgelas - gelas;
 	if stokgelas < 1 then
 	begin
-		restockgelas := 10000*5;
-		stokgelas := stokgelas + 100;
-	end;
+		restockgelas := 50000;
+		stokgelas := 100 + stokgelas;
+	end else stokgelas := 0 + stoklemon;
+	stoksedotan := stoksedotan - sedotan;
 	if stoksedotan < 1 then
 	begin
 		restocksedotan := 10000;
-		stoksedotan := stoksedotan + 100;
-	end;
-	writeln("Stok Bahan Lemonade Yay");
-	writeln("Stok Gula : ", stokgula);
-	writeln("Stok Lemon : ", stoklemon);
-	writeln("Stok Gelas : ", stokgelas);
-	writeln("Stok Sedotan : ", stoksedotan);
-	writeln("Pengeluaran Restock = Rp ", restocklemon+restockgula+restockgelas+restocksedotan);
-end.
+		stoksedotan := 100 + stoksedotan;
+	end else stoksedotan := 0 + stoksedotan;
+	restockvalue := restockvalue+restocklemon+restockgula+restockgelas+restocksedotan;
+	writeln('Stok Bahan Lemonade Yay');
+	writeln('Stok Gula : ', stokgula);
+	writeln('Stok Lemon : ', stoklemon);
+	writeln('Stok Gelas : ', stokgelas);
+	writeln('Stok Sedotan : ', stoksedotan);
+	writeln('Pengeluaran Restock = Rp ', restockvalue);
+end;
 
 
-procedure Payroll(time : integer);
+procedure Payroll(time : longint);
 var
-	perjam, gaji : integer;
+	perjam, gaji : longint;
 begin
 	perjam := 20000;
 	gaji := time*perjam;
-	writeln("Hari ini anda mendapatkan pendapatan sebesar Rp ",gaji); {Pay Employee}
-end.
+	writeln('Hari ini anda mendapatkan pendapatan sebesar Rp ',gaji); {Pay Employee}
+end;
 
 {ALGORITMA UTAMA}
 begin
-		writeln("Stok hari ini :");
-		write("Stok Gula : "); readln(stokgula);
-		write("Stok Lemon : "); readln(stoklemon);
-		write("Stok Gelas : "); readln(stokgelas);
-		write("Stok Sedotan : "); readln(stoksedotan);
+		writeln('Stok hari ini :');
+		write('Stok Gula : '); readln(stokgula);
+		write('Stok Lemon : '); readln(stoklemon);
+		write('Stok Gelas : '); readln(stokgelas);
+		write('Stok Sedotan : '); readln(stoksedotan);
+		restockvalue := 0;
 		time := 0;
-		writeln("------------------------------------------------");
-		writeln("------------JUALAN LEMONADE YAY-----------------");
-		writeln("------------------------------------------------");
-		writeln("1. Ada yang mau beli nih...");
-		writeln("2. Udahan ah jualannya...");
-		write("Pilih yang mana : ");
+		writeln('------------------------------------------------');
+		writeln('--------------JUALAN LEMONADE YAY---------------');
+		writeln('------------------------------------------------');
+		writeln('1. Ada yang mau beli nih...');
+		writeln('2. Udahan ah jualannya...');
+		write('Pilih yang mana : ');
 		readln(pil);
 		if pil = 1 then
 		begin
-			while pil<>2 do
+			while pil=1 do
 			begin
-				write("Mau beli berapa? ");
+				write('Mau beli berapa? ');
 				readln(n);
 				Sale(n, harga, jumlah);
 				time := time + 1;
-				Production(n, harga, stokgula, stoklemon, stokgelas, stoksedotan);
-				writeln("-------------------------------------------------");
-				writeln("Menu Awal :");
-				writeln("1. Ada yang mau beli nih...");
-				writeln("2. Udahan ah jualannya...");
-				write("Pilih yang mana : ");
+				Production(n, stokgula, stoklemon, stokgelas, stoksedotan, restockvalue);
+				writeln('----------------------------------------------');
+				writeln('Menu Awal :');
+				writeln('1. Ada yang mau beli nih...');
+				writeln('2. Udahan ah jualannya...');
+				write('Pilih yang mana : ');
 				readln(pil);
 			end;
 		end;
 		Payroll(time);
+		writeln('Press any key to continue...');
+		readln(key);
 end.
 	
